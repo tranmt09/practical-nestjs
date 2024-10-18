f84dec7983e364f773af2ec057877d3b522de14a
Tam Tran Minh <TRANMT09@heiway.net>
2024-10-18 16:02:13 +0700

    minor: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,14 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin', '@trivago/prettier-plugin-sort-imports'],
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    '@trivago/prettier-plugin-sort-imports',
+  ],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   root: true,
   env: {
     node: true,
@@ -18,5 +24,19 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { selector: 'interface', format: ['PascalCase'], prefix: ['I'] },
+      {
+        selector: 'variable',
+        type: 'boolean',
+        format: ['PascalCase'],
+        prefix: ['is', 'should', 'has', 'can', 'did', 'will'],
+      },
+    ],
   },
 };
